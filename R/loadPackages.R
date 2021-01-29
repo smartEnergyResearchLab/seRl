9007492c372dbe7baa45ae827ffeed92cca31f5d
Ben Anderson <dataknut@icloud.com>
2021-01-29 14:38:40 +0000

    stub for loadPackages

---
+++ R/loadPackages.R
@@ -0,0 +1,4 @@
+# function to load R packages on the SERL AWS instance
+# Checks to see if the packages already exist
+# if not, installs them in local user space from the SERL R package CRAN mirror
+# then loads them
